bfcf9ea559100eb0c33456968c8eb3cbcb8a2f8f
Mr-animesh <janimesh936@gmail.com>
2025-09-18 21:50:49 +0530

    please work

--- api/server.js
+++ api/server.js
@@ -83,7 +83,7 @@ io.on("connection", (socket) => {
 
 // Handle React routing in production
 if (process.env.NODE_ENV === "production") {
-    app.get("*", (req, res) => {
+    app.get("/{*splat}", (req, res) => {
         res.sendFile(path.join(__dirname, '../client','dist', 'index.html'));
     });
 }
